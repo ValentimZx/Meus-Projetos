eaeb19ecce77eab916cc10cc1d2ef42d63b3088d
ValentimZx <nvalentim824@gmail.com>
2025-06-29 20:11:56 -0300

    Commit dos meus projetos

--- Projeto-Cadastro-De-Produtos/Program.cs
+++ Projeto-Cadastro-De-Produtos/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Digite o nome do produto: \n");
+var produto = Console.ReadLine();
+
+Console.WriteLine("Digite o preco do produto: \n");
+double preco = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite a quantidade do produto:\n ");
+int quantidade = Convert.ToInt32(Console.ReadLine());
+
+double total = preco * quantidade;
+if (total > 100)
+{
+    double desconto = total * 0.10;
+    double totalComDeconto = total - desconto;
+
+    Console.WriteLine($"\nProduto: {produto}");
+    Console.WriteLine($"Valor total sem desconto: R$ {total:F2}");
+    Console.WriteLine($"Compra acima de R$100! Desconto de 10% aplicado.");
+    Console.WriteLine($"Valor com desconto; R$ {totalComDeconto}");
+}
+else
+{
+    Console.WriteLine($"\nProduto: {produto}");
+    Console.WriteLine($"Valor total da sua compra: R${total:F2}");
+}
---
+++ Sistema-De-Caixa/Program.cs
@@ -0,0 +1,47 @@
+﻿bool condicao = true;
+double totalAcumulado = 0;
+
+while (condicao)
+{
+    Console.WriteLine("\nMenu:");
+    Console.WriteLine("1 - Comprar produto");
+    Console.WriteLine("2 - Ver total acumulado");
+    Console.WriteLine("3 - Sair");
+
+    Console.Write("Escolha uma opcao:");
+    var menu = Console.ReadLine();
+    switch (menu)
+    {
+        case "1":
+            Console.WriteLine("\n-- Comprar produto --");
+            Console.Write("Nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preco do produto: ");
+            var preco = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Quantidade: ");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+            double subTotal = preco * quantidade;
+            totalAcumulado += subTotal;
+
+            Console.WriteLine($"Produto '{nome}' adicionado. Subtotal: R$ {subTotal:F2}");
+            Console.WriteLine($"Total parcial: R$ {totalAcumulado:F2}");
+            break;
+
+        case "2":
+            Console.WriteLine($"\nTotal Acumulad: R${totalAcumulado}");
+            break;
+
+        case "3":
+            Console.WriteLine("\nObrigado pela compra!");
+            condicao = false;
+            break;
+
+        default:
+            Console.WriteLine("Opcao invalida, tente novamente!");
+            break;
+
+    }
+}
